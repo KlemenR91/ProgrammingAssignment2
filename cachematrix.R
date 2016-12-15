e4ae8ae392b194b44cee10cfc88c2fdd3d83d4d7
KlemenR91 <klemen.rizman@gmail.com>
2016-12-15 15:50:57 +0100

    Comments
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,32 +4,33 @@
 ## This function creates new matrix with getter and setter functions
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    m <- NULL #INITIALIZE m
     set <- function(y) {
+        #FUNCTION SETS ATRIBUTE VALUE TO X AND NULL TO M
         x <<- y
         m <<- NULL
     }
-    get <- function() x
-    setinverse <- function(inverse) m <<- inverse
-    getinverse <- function() m
+    get <- function() x #RETURN VALUE OF MATRIX
+    setinverse <- function(inverse) m <<- inverse  #SETS INVERSE VALUE TO SET INVERSE
+    getinverse <- function() m #RETURNS INVERSE VALUE
     list(set = set, get = get,
          setinverse = setinverse,
-         getinverse = getinverse)
+         getinverse = getinverse) 
 }
 
 
 ## This function returns inverse of matrix
 
 cacheSolve <- function(x, ...) {
-    m <- x$getinverse()
-    if(!is.null(m)) {
+    m <- x$getinverse()   #SET VALUE INVERSE VALUE TO M
+    if(!is.null(m)) { #IF INVERSE VALUE ALREADY EXISTS
         message("getting cached data")
         return(m)
-    }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinverse(m)
-    m
+    } #IF INVERSE DOESN'T EXISTS
+    data <- x$get() #GET MATRIX VALUE
+    m <- solve(data, ...) #CALCULATE INVERSE
+    x$setinverse(m) #SET INVERSE TO X VARIABLE
+    m 
 }
 
 #If you want to test this program:
